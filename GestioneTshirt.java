7a1c22a49bd5f98ad437ccbf4eb07223cd3ab3bd
SaraAbb <saraabbonizio@live.it>
2016-07-07 13:00:17 +0200

    Update GestioneTshirt.java

--- GestioneTshirt.java
+++ GestioneTshirt.java
@@ -1,4 +1,9 @@
-package Magazzino;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package magazzino;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,22 +13,33 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class GestioneBorse {
+/**
+ *
+ * @author Sara
+ */
+public class GestioneTshirt {
     
-    public GestioneBorse()
+    public GestioneTshirt()
     {
-    
+        
     }
     
-    public void inserisciBorsa(int idborsa, String mod,String col,float pb,int q)
+    public void inserisciMaglia(int idmaglia,String gen,String col,int s,int m,int l,int xl,String scol,String maniche,String mat,float pb)
     {
         try{
             Connection conn= DriverManager.getConnection("jdbc:mysql://localhost/cherryqueen", "root", "");
-        PreparedStatement pst=conn.prepareStatement("INSERT INTO borse(Modello,Colore,PrezzoBase,Quantità) VALUES(?,?,?,?)");
-        pst.setString(1,mod);
-        pst.setString(2,col);
-        pst.setFloat(3,pb);
-        pst.setInt(4,q);
+        PreparedStatement pst=conn.prepareStatement("INSERT INTO maglia(IDmaglia,Genere,Colore,S,M,L,XL,Scollatura,Maniche,Materiale,PrezzoBase) VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+        pst.setInt(1,idmaglia);
+        pst.setString(2,gen);
+        pst.setString(3,col);
+        pst.setInt(4,s);
+        pst.setInt(5,m);
+        pst.setInt(6,l);
+        pst.setInt(7,xl);
+        pst.setString(8,scol);
+        pst.setString(9,maniche);
+        pst.setString(10,mat);
+        pst.setFloat(11,pb);
         pst.executeUpdate();
         conn.close();
         System.out.println("Fatto");
@@ -33,19 +49,43 @@ public class GestioneBorse {
      }
     }
     
-    public void cambiaQuantitaBorsa(int id,int q) //cambia la quantità
+    public int getQuantitaAttuale(int id, String taglia)
+    {
+        try{
+        Connection conn= DriverManager.getConnection("jdbc:mysql://localhost/cherryqueen", "root", "");
+        Statement st=conn.createStatement();
+        ResultSet res=st.executeQuery("SELECT * FROM maglia WHERE IDmaglia="+id+""); //gli spazi sono importanti!!
+        //altrimenti è come scrivere FROMtabella che non è un comando sql
+        
+        if(res.next())
+        {
+            int t=res.getInt(taglia);
+            System.out.println(""+taglia+": "+t+"");
+            return t;
+        }
+        res.close(); //chiudere prima res
+        conn.close(); //poi connessione
+        }catch(SQLException ex)
+        {
+            System.out.println("Errore SQL!");
+            ex.printStackTrace();
+        }
+        return -1;
+    }
+    
+    public void cambiaQuantitaMaglia(int id,int q,String taglia) //cambia la quantità
     // NB: AGGIORNA, NON SOTTRAE! Se ho 20 maglie e ne ordino 5, 
     //alla funzione dovrò passare 15, il risultato già della sottrazione
     //la sottrazione sarà fatta dalla riga di codice precedente al richiamo della funzione
     {
         try{ 
-        int qatt=this.getQuantitaAttuale(id);
+        int qatt=this.getQuantitaAttuale(id,taglia);
         if(q<=qatt)
         {
             //questa parte funziona quella prima di controllo no, ma il compilatore
             //non da' errori, eseguendolo da solo Errore Sql
         Connection conn= DriverManager.getConnection("jdbc:mysql://localhost/cherryqueen", "root", "");
-        PreparedStatement pst=conn.prepareStatement("UPDATE borse SET Quantità=? WHERE IDborsa=?");
+        PreparedStatement pst=conn.prepareStatement("UPDATE maglia SET "+taglia+"=? WHERE IDmaglia=?");
         pst.setInt(1,q);
         pst.setInt(2,id);
         pst.executeUpdate();
@@ -61,36 +101,12 @@ public class GestioneBorse {
         }
     }
     
-    public int getQuantitaAttuale(int id)
-    {
-        try{
-        Connection conn= DriverManager.getConnection("jdbc:mysql://localhost/cherryqueen", "root", "");
-        Statement st=conn.createStatement();
-        ResultSet res=st.executeQuery("SELECT * FROM borse WHERE IDborse="+id+""); //gli spazi sono importanti!!
-        //altrimenti è come scrivere FROMtabella che non è un comando sql
-        
-        if(res.next())
-        {
-            int t=res.getInt("Quantità");
-            System.out.println("Quantità: "+t+"");
-            return t;
-        }
-        res.close(); //chiudere prima res
-        conn.close(); //poi connessione
-        }catch(SQLException ex)
-        {
-            System.out.println("Errore SQL!");
-            ex.printStackTrace();
-        }
-        return -1;
-    }
-    
-    public void eliminaBorsa(int id)
+    public void eliminaMaglia(int id)
     {
         try{
             Connection conn= DriverManager.getConnection("jdbc:mysql://localhost/cherryqueen", "root", "");
             Statement st=conn.createStatement();
-            ResultSet rs=st.executeQuery("DELETE from borse WHERE IDborse="+id+"");
+            ResultSet rs=st.executeQuery("DELETE from maglia WHERE IDmaglia="+id+"");
         }catch(SQLException s)
         {
             System.out.println("Errore SQL!");
@@ -98,12 +114,12 @@ public class GestioneBorse {
         }
     }
     
-    public void visualizzaBorse()
+    public void visualizzaMaglie()
      {
          try{
              Connection conn= DriverManager.getConnection("jdbc:mysql://localhost/cherryqueen", "root", "");
              Statement st=conn.createStatement();
-             ResultSet rs=st.executeQuery("SELECT * from borse");
+             ResultSet rs=st.executeQuery("SELECT * from maglia");
              ResultSetMetaData rm=rs.getMetaData();
              int numColonne=rm.getColumnCount();
              for(int i=1; i<=numColonne;i++)
@@ -126,15 +142,15 @@ public class GestioneBorse {
              System.out.println("Errore SQL!");
          }
      }
-    
+     
      public int getLastID()
     {
         try{
         Connection conn= DriverManager.getConnection("jdbc:mysql://localhost/cherryqueen", "root", "");
              Statement st=conn.createStatement();
-             ResultSet rs=st.executeQuery("SELECT IDborse from borse");
+             ResultSet rs=st.executeQuery("SELECT IDmaglia from maglia");
              rs.last();
-             int lastid=rs.getInt("IDborse");
+             int lastid=rs.getInt("IDmaglia");
              return lastid;
         }catch(SQLException ex)
         {
@@ -143,5 +159,4 @@ public class GestioneBorse {
         }
         return -1;
     }
-    
 }
